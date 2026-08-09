6e915deb00159639e74d5da715e73f0ab0632a72
Samson Ikilama <mayindiikilama@gmail.com>
2026-08-09 12:25:59 -0400

    Added EF Core DbContext and database connection setup

---
+++ PS5GameManagementSystem/SIGameCatalogueService/Data/GameDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIGameCatalogueService.Models;
+
+namespace SIGameCatalogueService.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
