7bced109d3fd25e8a154a80abec6d3069140fc00
Samson Ikilama <mayindiikilama@gmail.com>
2026-08-09 12:21:35 -0400

    Added Game model to represent game data

---
+++ PS5GameManagementSystem/SIGameCatalogueService/Models/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SIGameCatalogueService.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = "";
+
+        public string Genre { get; set; } = "";
+
+        public string Publisher { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public bool InStock { get; set; }
+    }
+}
+
