70addd65a085ff0d6e342e4e8f283c6a5d59edd8
Samson Ikilama <mayindiikilama@gmail.com>
2026-08-10 21:58:20 -0400

    Refactor project and add database integration Configured `Program.cs` for database integration with SQL Server and added `LibraryDbContext` for managing database interactions. Introduced `GameLibrary` model to represent game library entities.  Added database connection string to `appsettings.json`.

---
+++ PS5GameManagementSystem/SILibraryService/Data/LibraryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SILibraryService.Models;
+
+namespace SILibraryService.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GameLibrary> GameLibraries => Set<GameLibrary>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GameLibrary>()
+                .Property(g => g.Price)
+                .HasPrecision(10, 2);
+        }
+    }
+}
---
+++ PS5GameManagementSystem/SILibraryService/Models/GameLibrary.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SILibraryService.Models
+{
+    public class GameLibrary
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = "";
+
+        public int GameId { get; set; }
+
+        public string GameTitle { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
