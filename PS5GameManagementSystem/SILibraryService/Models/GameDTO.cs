546c112294be779df358c641be0ecb2e69a89250
Samson Ikilama <mayindiikilama@gmail.com>
2026-08-16 13:28:28 -0400

    Add GameDTO class for game data representation

---
+++ PS5GameManagementSystem/SILibraryService/Models/GameDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace SILibraryService.Models
+{
+    public class GameDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = "";
+
+        public string Genre { get; set; } = "";
+
+        public string Publisher { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public bool InStock { get; set; }
+    }
+}
