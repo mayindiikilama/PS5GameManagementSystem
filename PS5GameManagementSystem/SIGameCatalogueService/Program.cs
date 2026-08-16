00b67d48659187ecabc20ff11ac301c8159531bc
Samson Ikilama <mayindiikilama@gmail.com>
2026-08-16 16:35:08 -0400

    Add PostgreSQL support alongside SQL Server
    Updated `Program.cs` to support both PostgreSQL and SQL Server.
    Added logic to configure `GameDbContext` based on the presence
    of the `DATABASE_URL` environment variable for PostgreSQL or
    fall back to SQL Server for local development. Included the
    `Npgsql.EntityFrameworkCore.PostgreSQL` package in the project
    to enable Entity Framework Core support for PostgreSQL.

--- PS5GameManagementSystem/SIGameCatalogueService/Program.cs
+++ PS5GameManagementSystem/SIGameCatalogueService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using SIGameCatalogueService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,36 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<GameDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+// Check if Render provides a PostgreSQL DATABASE_URL
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    // Render PostgreSQL connection
+    var databaseUri = new Uri(databaseUrl);
+
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    var connectionString = new NpgsqlConnectionStringBuilder
+    {
+        Host = databaseUri.Host,
+        Port = databaseUri.Port,
+        Database = databaseUri.AbsolutePath.TrimStart('/'),
+        Username = Uri.UnescapeDataString(userInfo[0]),
+        Password = Uri.UnescapeDataString(userInfo[1]),
+        SslMode = SslMode.Require
+    }.ConnectionString;
+
+    builder.Services.AddDbContext<GameDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+else
+{
+    // Local development / Docker SQL Server
+    builder.Services.AddDbContext<GameDbContext>(options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+}
 
 
 var app = builder.Build();
